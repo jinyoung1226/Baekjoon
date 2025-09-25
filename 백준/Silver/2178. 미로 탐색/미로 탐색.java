b4cebe773ca66b1a4e5365d027fbb565cc154f8a
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2025-09-26 01:26:53 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 16640 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,16 +2,26 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 
+
 public class Main {
 
-    static int[][] map;
+    static class Point {
+        int x;
+        int y;
+        int dist;
+
+        public Point(int x, int y, int dist) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    }
+
+    static int N, M;
+    static int[][] graph;
     static boolean[][] visited;
-    static int num;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int N;
-    static int M;
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,46 +31,42 @@ public class Main {
         N = Integer.parseInt(data[0]);
         M = Integer.parseInt(data[1]);
 
-        map = new int[N][M];
+        graph = new int[N][M];
         visited = new boolean[N][M];
-        num = 0;
 
         for (int i = 0; i < N; i++) {
-            String[] data1 = br.readLine().split("");
+            String[] str = br.readLine().split("");
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(data1[j]);
+                graph[i][j] = Integer.parseInt(str[j]);
             }
         }
-
-        BFS(0,0);
-        System.out.println(map[N - 1][M - 1]);
+        bfs();
     }
 
-    static void BFS(int x, int y) {
-        Queue<int []> queue = new LinkedList<>();
-        queue.add(new int[] {x,y});
-        visited[x][y] = true;
+    static void bfs() {
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(0,0,1));
+        visited[0][0] = true;
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
 
-            int now[] = queue.poll();
-            int nowX = now[0];
-            int nowY = now[1];
+            if (point.x == N-1 && point.y == M-1) {
+                System.out.println(point.dist);
+                break;
+            }
 
             for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
+                int newX = point.x + dx[i];
+                int newY = point.y + dy[i];
 
-                if(nextX >= 0 && nextY >= 0 && nextX < N && nextY < M) {
-                    if(!visited[nextX][nextY] && map[nextX][nextY] == 1) {
-                        queue.add(new int[] {nextX, nextY});
-                        visited[nextX][nextY] = true;
-                        map[nextX][nextY] = map[nowX][nowY] + 1;
+                if (newX >= 0 && newY >= 0 && newX < N && newY < M) {
+                    if (!visited[newX][newY] && graph[newX][newY] == 1) {
+                        visited[newX][newY] = true;
+                        queue.add(new Point(newX, newY, point.dist + 1));
                     }
                 }
             }
-         }
-
+        }
     }
-
 }
