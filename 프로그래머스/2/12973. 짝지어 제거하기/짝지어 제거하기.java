acffadfd9737ad4a6c997138f3ec812a29732bd6
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2025-11-25 14:51:31 +0900

    [level 2] Title: 짝지어 제거하기, Time: 66.02 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(arr[i]);
+            } else if (stack.peek() == arr[i]) {
+                stack.pop();
+            } else {
+                stack.push(arr[i]);
+            }
+        }
+        
+        if (stack.isEmpty()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
