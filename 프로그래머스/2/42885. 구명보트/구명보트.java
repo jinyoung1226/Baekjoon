776246afb2b5cbea54be39508832f7541be582af
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2025-11-26 17:51:04 +0900

    [level 2] Title: 구명보트, Time: 11.89 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int count = 0;
+        
+        int start = 0;
+        int end = people.length - 1;
+        
+        while (start <= end) {
+            if (people[start] + people[end] <= limit) {
+                start++;
+                end--;
+            } else {
+                end--; 
+            }
+            count++;
+        }
+        
+        return count;
+    }
+}
