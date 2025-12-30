499c16856750d11b2e0efd673601969ad6043c48
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2025-12-30 21:22:36 +0900

    [Silver II] Title: N과 M (12), Time: 172 ms, Memory: 15916 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+    static int[] tmp_arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] data = br.readLine().split(" ");
+        N = Integer.parseInt(data[0]);
+        M = Integer.parseInt(data[1]);
+
+        arr = new int[N];
+        tmp_arr = new int[M];
+        String[] data1 = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(data1[i]);
+        }
+
+        Arrays.sort(arr);
+
+        comb(0, 0);
+    }
+
+    static void comb(int start, int depth) {
+        if (depth == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < tmp_arr.length; i++) {
+                sb.append(tmp_arr[i]).append(" ");
+            }
+
+            System.out.println(sb);
+            return;
+        }
+
+        int before = -1;
+
+        for (int i = start; i < N; i++) {
+            if (before != arr[i]) {
+                before = arr[i];
+                tmp_arr[depth] = arr[i];
+                comb(i, depth + 1);
+            }
+        }
+    }
+}
