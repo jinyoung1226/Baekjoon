179e25fdb61f59261033156eae7b3d36bc169266
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-01-24 12:42:36 +0900

    [level 2] Title: 전화번호 목록, Time: 306.19 ms, Memory: 99.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        
+        if (phone_book.length == 1) {
+            return true;
+        }
+        
+        Arrays.sort(phone_book);
+
+        
+        boolean result = true;
+        
+        for (int i = 0; i < phone_book.length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                result = false;
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
