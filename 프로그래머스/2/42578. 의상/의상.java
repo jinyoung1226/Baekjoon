0085d3dd40f27c9159d8369eb84e72e14dc76872
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-05-14 23:56:44 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,23 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> clothCount = new HashMap<>();
         
-        System.out.println(clothes.length);
-        for (int i = 0; i < clothes.length; i++) {
-            String clothName = clothes[i][0];
-            String clothKind = clothes[i][1];
-            
-            map.put(clothKind, map.getOrDefault(clothKind, 0) + 1);
+        for (String[] cloth : clothes) {
+            String key = cloth[1];
+            clothCount.put(key, clothCount.getOrDefault(key, 0) + 1);
         }
         
-        System.out.println(map);
-        
-        int count = 1;
-        for (String key : map.keySet()) {
-            count *= map.get(key)+ 1;
+        int result = 1;
+        for (Integer count : clothCount.values()) {
+            result *= (count+1);
         }
         
-        return count-1;
+        return result - 1;
     }
 }
