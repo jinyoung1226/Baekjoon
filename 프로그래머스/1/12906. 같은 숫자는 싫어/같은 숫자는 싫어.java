9a02661eaadc72a7bbcc1c2326fc9282c1ee5111
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-05-16 17:24:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 23.96 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,26 +2,27 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(arr[0]);
+        
         List<Integer> list = new ArrayList<>();
-        
-        int prev = arr[0];
-        list.add(prev);
-        
         for (int i = 1; i < arr.length; i++) {
-            if (prev != arr[i]) {
-                list.add(arr[i]);
-                prev = arr[i];
+            if (queue.peek() == arr[i]) {
+                continue;
+            } else {
+                list.add(queue.poll());
+                queue.add(arr[i]);
             }
         }
         
+        list.add(queue.poll());
+        
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
+        
+        for (int i = 0 ; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
-        
 
         return answer;
-        
-        
     }
 }
