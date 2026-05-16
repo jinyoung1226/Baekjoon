5a7c830f5b9f0f7f6eaf4b85943ac79313200ec8
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-05-16 21:09:47 +0900

    [level 2] Title: 올바른 괄호, Time: 8.89 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,32 +1,26 @@
 import java.util.*;
+
 class Solution {
     boolean solution(String s) {
-        Stack<String> stack = new Stack<>();
-        char[] arr = s.toCharArray();
-        String[] arr1 = new String[arr.length];
+        Stack<Character> stack = new Stack<>();
+        boolean answer = true;
         
-        for (int i = 0; i < arr.length; i++) {
-            arr1[i] = String.valueOf(arr[i]);
-        }
-        
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i].equals("(")) {
-                stack.push(arr1[i]);
-            }
-            
-            if (arr1[i].equals(")")) {
-                if (stack.isEmpty()) {
-                    return false;
-                } else {
-                    stack.pop();
-                }
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else if (!stack.isEmpty() && ch == ')') {
+                stack.pop();
+            } else if (stack.isEmpty() && ch == ')') {
+                answer = false;
+                break;
             }
         }
         
-        if (stack.isEmpty()) {
-            return true;
-        } else {
-            return false;
+        if (!stack.isEmpty()) {
+            answer = false;
         }
+        
+
+        return answer;
     }
 }
