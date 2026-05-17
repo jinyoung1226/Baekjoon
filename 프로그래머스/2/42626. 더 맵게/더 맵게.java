3391aa793212f6192b99cbfdf7852bb169c2c8eb
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-05-17 19:14:48 +0900

    [level 2] Title: 더 맵게, Time: 892.65 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
+        
+        int answer = 0;
+        
+        while (pq.peek() < K) {
+            if (pq.size() == 1) {
+                answer = -1;
+                break;
+            }
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            int newScoville = first + (second * 2);
+            pq.add(newScoville);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
