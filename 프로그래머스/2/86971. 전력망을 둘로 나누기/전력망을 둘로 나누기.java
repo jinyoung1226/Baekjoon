90fd2c867fb62b6512a19809aa6ed53ec8216d78
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-05-23 23:15:25 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.20 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,56 +1,52 @@
 import java.util.*;
 
 class Solution {
-    
-    static List<Integer>[] arr;
-    static boolean[] visited;
-    static Integer result = Integer.MAX_VALUE;
+    List<Integer>[] graph;
+    boolean[] visited;
+    Integer max = Integer.MAX_VALUE;
     
     public int solution(int n, int[][] wires) {
-        
-        int jump = 0;
-        for (int i = 0; i < wires.length; i++) {
-            
-            // 인접리스트 초기화
-            arr = new ArrayList[n+1];
-            for (int j = 0; j <= n; j++) {
-                arr[j] = new ArrayList<>();
-            }
-            
-            // 하나씩 뺀 걸 기반으로 BFS로 탐색
-            for (int j = 0; j < wires.length; j++) {
-                if (jump == j) continue;
-                
-                int first = wires[j][0];
-                int second = wires[j][1];
-                arr[first].add(second);
-                arr[second].add(first);
-            }
-            
-            
-            // BFS 탐색
-            int firstCount = BFS(1);
-            int secondCount = Math.abs(n-firstCount);
-            int lastCount = Math.abs(firstCount - secondCount);
-            
-            
-            result = Math.min(result,lastCount);
-            jump++;
+        graph = new ArrayList[n+1];
+        for (int i = 0; i < n+1; i++) {
+            graph[i] = new ArrayList<>();
         }
-        return result;
+        
+        for (int i = 0; i < wires.length; i++) {
+            int first = wires[i][0];
+            int second = wires[i][1];
+            graph[first].add(second);
+            graph[second].add(first);
+        }
+        
+        for (int i = 0; i < wires.length; i++) {
+            int first = wires[i][0];
+            int second = wires[i][1];
+            graph[first].remove(Integer.valueOf(second));
+            graph[second].remove(Integer.valueOf(first));
+            
+            visited = new boolean[n+1];
+            int count = bfs(1);
+            int anotherCount = n - count;
+            
+            max = Math.min(max, Math.abs(count - anotherCount));
+            graph[first].add(second);
+            graph[second].add(first);
+        }
+        
+        return max;
+
     }
     
-    public int BFS(int start) {
+    public int bfs(int start) {
         Queue<Integer> q = new LinkedList<>();
-        visited = new boolean[arr.length];
         q.add(start);
-        visited[start] = true;
         int count = 1;
+        visited[start] = true;
         
         while(!q.isEmpty()) {
             Integer now = q.poll();
             
-            for (Integer next : arr[now]) {
+            for (Integer next : graph[now]) {
                 if (!visited[next]) {
                     visited[next] = true;
                     q.add(next);
