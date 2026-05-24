179b76868c25be7c5ae6ce65ed31642e11706aa0
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-05-24 16:46:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.68 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,8 +1,13 @@
 import java.util.*;
 
 class Solution {
+    int[][] board;
+    boolean[][] visited;
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    int answer = 0;
     
-    static class Point {
+    class Point {
         int x;
         int y;
         int dist;
@@ -14,43 +19,47 @@ class Solution {
         }
     }
     
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    
     public int solution(int[][] maps) {
+        board = new int[maps.length][maps[0].length];
         visited = new boolean[maps.length][maps[0].length];
+        for (int i = 0; i < maps.length; i++) {
+            for (int j = 0; j < maps[0].length; j++) {
+                board[i][j] = maps[i][j];
+            }
+        }
         
-        int result = bfs(maps);
-        return result;
-        
+        if(bfs()) {
+            return answer;
+        } else {
+            return -1;
+        }
     }
     
-    static int bfs(int[][] maps) {
+    public boolean bfs() {
         Queue<Point> q = new LinkedList<>();
         q.add(new Point(0, 0, 1));
         visited[0][0] = true;
         
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             Point now = q.poll();
             
-            if (now.x == maps.length - 1 && now.y == maps[0].length - 1) {
-                return now.dist;
+            if (now.x == board.length-1 && now.y == board[0].length-1) {
+                answer = now.dist;
+                return true;
             }
             
             for (int i = 0; i < 4; i++) {
-                int nextX = now.x + dx[i];
-                int nextY = now.y + dy[i];
+                int newX = now.x + dx[i];
+                int newY = now.y + dy[i];
                 
-                if (nextX >= 0 && nextX < maps.length && nextY >= 0 && nextY < maps[0].length) {
-                    if (!visited[nextX][nextY] && maps[nextX][nextY] != 0) {
-                        visited[nextX][nextY] = true;
-                        q.add(new Point(nextX, nextY, now.dist + 1));
+                if (newX >= 0 && newY >= 0 && newX < board.length && newY < board[0].length) {
+                    if (!visited[newX][newY] && board[newX][newY] != 0) {
+                        visited[newX][newY] = true;
+                        q.add(new Point(newX, newY, now.dist + 1));
                     }
                 }
             }
         }
-        
-        return -1;
+        return false;
     }
 }
