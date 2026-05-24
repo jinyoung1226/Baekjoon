9d1d59366bcfc9ebf8dda509d1688815f0162c71
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-05-24 14:45:11 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,52 +1,50 @@
 import java.util.*;
 
 class Solution {
-    
-    static int[] parent;
+    List<Integer>[] graph;
+    boolean[] visited;
+    int count = 0;
     
     public int solution(int n, int[][] computers) {
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
         }
         
-        List<int[]> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < computers.length; i++) {
+            for (int j = 0; j < computers[i].length; j++) {
                 if (i == j) continue;
-                
                 if (computers[i][j] == 1) {
-                    list.add(new int[]{i,j});
+                    graph[i+1].add(j+1);
                 }
             }
         }
         
-        for (int i = 0; i < list.size(); i++) {
-            int[] data = list.get(i);
-            union(data[0], data[1]);
+        for (int i = 1; i < graph.length; i++) {
+            if (!visited[i]) {
+                bfs(i);
+                count++;
+            }
         }
-        
-        System.out.println(Arrays.toString(parent));
-        
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < parent.length; i++) {
-            set.add(find(i));
-        }
-        
-        
-        return set.size();
+
+        return count;
     }
     
-    public int find(int x) {
-        if (parent[x] == x) return x;
-        else return parent[x] = find(parent[x]);
-    }
-    
-    public void union(int x, int y) {
-        int nx = find(x);
-        int ny = find(y);
+    public void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
         
-        if (nx < ny) parent[ny] = nx;
-        else parent[nx] = ny;
+        while(!q.isEmpty()) {
+            Integer now = q.poll();
+            
+            for (Integer next : graph[now]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.add(next);
+                }
+            }
+        }
     }
 }
