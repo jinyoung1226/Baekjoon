c9072f853bb27908d09ce2aa6452716895c415d6
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-07-04 02:01:03 +0900

    [level 2] Title: 튜플, Time: 18.98 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -4,13 +4,15 @@ class Solution {
     public int[] solution(String s) {
         String[] arr = s.substring(2, s.length()-2).replace("},{", "-").split("-");
         Arrays.sort(arr, (o1, o2) -> o1.length() - o2.length());
+        HashSet<Integer> set = new HashSet<>();
         
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             String temp = arr[i];
             String[] s_arr = temp.split(",");
             for (int j = 0; j < s_arr.length; j++) {
-                if (!list.contains(Integer.valueOf(s_arr[j]))) {
+                if (!set.contains(Integer.valueOf(s_arr[j]))) {
+                    set.add(Integer.valueOf(s_arr[j]));
                     list.add(Integer.valueOf(s_arr[j]));
                 }
             }
