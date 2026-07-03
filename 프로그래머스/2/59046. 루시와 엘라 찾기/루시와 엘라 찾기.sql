606e0724377b9eaf9d5c6b7460032128dd085bf0
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-07-04 00:37:42 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,6 +1,6 @@
-SELECT 
+SELECT
     ANIMAL_ID,
     NAME,
     SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME IN ("Lucy", "Ella", "Pickle", "Rogan", "Sabrina", "Mitty")
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
