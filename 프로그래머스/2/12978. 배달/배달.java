2722685f2c62104f8fcb3a2562b908b2246d0aaa
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2026-07-23 21:47:25 +0900

    [level 2] Title: 배달, Time: 2.58 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -3,57 +3,58 @@ import java.util.*;
 class Solution {
     List<int[]>[] graph;
     int[] dist;
+    boolean[] visited;
     
     public int solution(int N, int[][] road, int K) {
         graph = new ArrayList[N+1];
-        for (int i = 0 ; i < graph.length; i++) {
+        dist = new int[N+1];
+        visited = new boolean[N+1];
+        
+        for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
+            dist[i] = Integer.MAX_VALUE;
         }
         
-        dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        
         for (int i = 0; i < road.length; i++) {
-            int node1 = road[i][0];
-            int node2 = road[i][1];
-            int cost = road[i][2];
-            
-            graph[node1].add(new int[]{node2, cost});
-            graph[node2].add(new int[]{node1, cost});
+            graph[road[i][0]].add(new int[]{road[i][1], road[i][2]});
+            graph[road[i][1]].add(new int[]{road[i][0], road[i][2]});
         }
         
         dijkstra(1);
         
-        int answer = 0;
-        for (int i = 1; i < dist.length; i++) {
-            if (dist[i] <= K) answer++;
+        int result = 0;
+        for (int i = 0; i < dist.length; i++) {
+            if (dist[i] <= K) {
+                result++;
+            }
         }
         
-        return answer;
+        return result;
+        
     }
     
     public void dijkstra(int start) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        dist[start] = 0;
         pq.add(new int[]{start, 0});
+        dist[start] = 0;
         
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             int[] now = pq.poll();
             
-            int currentNode = now[0];
-            int currentCost = now[1];
+            if (visited[now[0]]) {
+                continue;
+            } else {
+                visited[now[0]] = true;
+            }
             
-            if (dist[currentNode] > currentCost) continue;
-            
-            for (int[] next : graph[currentNode]) {
-                int nextNode = next[0];
-                int nextCost = next[1];
-                
-                if (dist[nextNode] > currentCost + nextCost) {
-                    dist[nextNode] = currentCost + nextCost;
-                    pq.add(new int[]{nextNode, dist[nextNode]});
+            for (int[] next : graph[now[0]]) {
+                if (dist[next[0]] > now[1] + next[1]) {
+                    dist[next[0]] = now[1] + next[1];
+                    pq.add(new int[]{next[0], dist[next[0]]});
                 }
             }
         }
     }
+    
+   
 }
